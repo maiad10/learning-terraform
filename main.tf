0bad6bf15a09b7cb768d94850ab1255962979ec1
maiad10 <91215810+maiad10@users.noreply.github.com>
2025-02-20 13:04:59 -0600

    Update security module

--- main.tf
+++ main.tf
@@ -41,6 +41,7 @@ resource "aws_instance" "blog" {
 
   subnet_id = module.blog_vpc.public_subnets[0]
   
+  
   tags = {
     Name = "Learning Terraform"
   }
@@ -49,13 +50,11 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-  name = "blog"
 
     vpc_id = module.blog_vpc.vpc_id
-
+    name = "blog"
     ingress_rules = ["http-80-tcp", "https-443-tcp"]
     ingress_cidr_blocks = ["0.0.0.0/0"]
-
     egress_rules = ["all-all"]
     egress_cidr_blocks = ["0.0.0.0/0"]
 }
