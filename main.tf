fea3275058a7ab5e5d8275b827c3b28341514ddc
maiad10 <91215810+maiad10@users.noreply.github.com>
2025-02-20 18:50:26 -0600

    Update load balancer

--- main.tf
+++ main.tf
@@ -54,19 +54,24 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
 
   # Security Group
-  security_groups = module.blog_sg.security_group_id 
+  security_groups = [module.blog_sg.security_group_id]
     
   }
 
-  target_groups = {
-    ex-instance = {
+  target_groups = [
+    {
       name_prefix      = "blog"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
+      targets = {
+        my_target = {
+          target_id        = aws_instance.blog.id
+          port = 80
+        }
+      }
     }
-  }
+  ]
 
   http_tcp_listeners = [
     {
